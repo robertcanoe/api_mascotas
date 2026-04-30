a9082a7bcbd59b2ea7de610372983bbabfca2c1f
robertcano <rcanoestevez@proton.me>
2026-04-30 13:55:39 +0200

    Add ensureMethod function to ApiController for HTTP method validation and update allowed methods in CORS headers

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -32,6 +32,18 @@ class ApiController extends BaseController
         }
     }
 
+    /**
+     * @param string ...$allowedMethods Nombres de metodo HTTP en mayusculas (p. ej. 'PUT', 'DELETE')
+     */
+    protected function ensureMethod(array $request, string ...$allowedMethods): void
+    {
+        $method = strtoupper((string) ($request['method'] ?? $_SERVER['REQUEST_METHOD'] ?? 'GET'));
+        $allowed = array_map('strtoupper', $allowedMethods);
+        if (!in_array($method, $allowed, true)) {
+            throw new HttpException(405, 'Method Not Allowed');
+        }
+    }
+
     protected function getRequestData(): array
     {
         return json_input();
@@ -41,7 +53,7 @@ class ApiController extends BaseController
     {
         header('Content-Type: application/json; charset=utf-8');
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
     }
 }
